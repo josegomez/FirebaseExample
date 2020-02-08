7327fe07bfe2336f79cd063d7c0256a3dffa8eaf
Jose C Gomez <jose@josecgomez.com>
2020-02-07 20:54:57 -0500

    Add project files.

---
+++ WebApp1/wwwroot/firebase-messaging-sw.js
@@ -0,0 +1,54 @@
+﻿importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-messaging.js');
+
+
+
+/**
+ * Here is is the code snippet to initialize Firebase Messaging in the Service
+ * Worker when your app is not hosted on Firebase Hosting.
+ // [START initialize_firebase_in_sw]
+ // Give the service worker access to Firebase Messaging.
+ // Note that you can only use Firebase Messaging here, other Firebase libraries
+ // are not available in the service worker.
+
+ // Initialize the Firebase app in the service worker by passing in the
+ // messagingSenderId.
+ firebase.initializeApp({
+   'messagingSenderId': 'YOUR-SENDER-ID'
+ });
+ // Retrieve an instance of Firebase Messaging so that it can handle background
+ // messages.
+ const messaging = firebase.messaging();
+ // [END initialize_firebase_in_sw]
+ **/
+var firebaseConfig = {
+    apiKey: "AIzaSyBQA1Nf5sQEJRTyupEzzQYHbvxi8JDVWUQ",
+    authDomain: "corodva-sample.firebaseapp.com",
+    databaseURL: "https://corodva-sample.firebaseio.com",
+    projectId: "corodva-sample",
+    storageBucket: "corodva-sample.appspot.com",
+    messagingSenderId: "625428422861",
+    appId: "1:625428422861:web:108444d31cbc70784f4984",
+    measurementId: "G-N1KJFSDN0T"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const messaging = firebase.messaging();
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// [START background_handler]
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
